2c804d38d99b0c30dea0c6165bfcc931b4b6a7ca
OUAHOUDA OULFA <o.ouahouda@esisa.ac.ma>
2023-04-23 00:07:33 +0000

    màj vue js

---
+++ whs_frontend/src/components/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <h1>Authentification</h1>
+      <form @submit.prevent="onSubmit">
+        <div>
+          <label for="username">Nom d'utilisateur :</label>
+          <input id="username" v-model="username" required>
+        </div>
+        <div>
+          <label for="password">Mot de passe :</label>
+          <input id="password" type="password" v-model="password" required>
+        </div>
+        <button type="submit">Se connecter</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  import { login } from '@/api';
+  
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+      };
+    },
+    methods: {
+      async onSubmit() {
+        try {
+          await login(this.username, this.password);
+          // Redirigez vers la page d'accueil de l'utilisateur après une connexion réussie
+          this.$router.push('/home');
+        } catch (error) {
+          // Affichez l'erreur si la connexion échoue
+          alert("Erreur d'authentification.");
+        }
+      },
+    },
+  };
+  </script>
+  
---
+++ whs_frontend/src/components/UserHome.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <div>
+      <h1>Bienvenue dans votre espace personnel</h1>
+      <!-- Ajoutez ici les fonctionnalités supplémentaires (recherche, téléchargement, déconnexion, etc.) -->
+      <ul>
+        <li v-for="item in directoryContent" :key="item.name">
+          {{ item.name }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { getDirectoryData } from '@/api';
+  
+  export default {
+    data() {
+      return {
+        directoryContent: [],
+      };
+    },
+    async created() {
+      try {
+        this.directoryContent = await getDirectoryData();
+      } catch (error) {
+        // Gérez l'erreur si la récupération du contenu du dossier échoue
+        alert("Erreur lors de la récupération du contenu du dossier.");
+      }
+    },
+  };
+  </script>
+  
