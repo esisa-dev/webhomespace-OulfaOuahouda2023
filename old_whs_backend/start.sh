d1e4c322c86490aff05118bc2c27f94d4e081fc4
OUAHOUDA OULFA <o.ouahouda@esisa.ac.ma>
2023-04-24 22:31:06 +0100

    qfqsdfqsd

--- old_whs_backend/start.sh
+++ old_whs_backend/start.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-
-# Active l'environnement virtuel
+python3 -m venv venv
 source venv/bin/activate
 
-# Définit les variables d'environnement pour Flask
-export FLASK_APP=app.py
-export FLASK_ENV=development
 
-# Démarre le serveur Flask
-flask run --port 5001
-
-
-
-
+ python3 app.py
